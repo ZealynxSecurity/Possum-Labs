9e57f58563b1ed6710952cc3d4d552fb6028f7d4
vendrell46 <vendrell_46@hotmail.com>
2024-03-31 13:04:28 +0200

    removeOwner

--- src/V2MultiAsset/Echidna/EchidnaVirtualLP.sol
+++ src/V2MultiAsset/Echidna/EchidnaVirtualLP.sol
@@ -7,65 +7,104 @@ contract EchidnaVirtualLP is EchidnaSetup {
 
     constructor() payable {}
 
+    // function register(
+    //     address testPortal,
+    //     address testAsset,
+    //     address testVault,
+    //     uint256 testPid
+    // ) private {
+    //     hevm.prank(psmSender);
+    //     virtualLP.registerPortal(
+    //         testPortal, 
+    //         testAsset, 
+    //         testVault, 
+    //         testPid
+    //     );
+    // }
+
+    // /**
+    //     REGISTER PORTAL
+    //  */
+    // /////////////// UNIT TESTS ///////////////
+    // function test_register_portal_usdc() public {
+    //     address testPortal = address(portal_USDC);
+    //     address testAsset = _PRINCIPAL_TOKEN_ADDRESS_USDC; 
+    //     address testVault = address(USDC_WATER);
+    //     uint256 testPid = _POOL_ID_USDC;
+
+    //     register(
+    //         testPortal, 
+    //         testAsset, 
+    //         testVault, 
+    //         testPid
+    //     );
+
+    //     // Validate the outcome
+    //     assert(virtualLP.registeredPortals(testPortal) == true);
+    //     assert(virtualLP.vaults(testPortal, testAsset) == testVault);
+    //     assert(virtualLP.poolID(testPortal, testAsset) == testPid);    
+    // }
+
+    // function test_register_portal_eth() public {
+    //     address testPortal = address(portal_ETH);
+    //     address testAsset = _PRINCIPAL_TOKEN_ADDRESS_ETH; 
+    //     address testVault = address(WETH_WATER);
+    //     uint256 testPid = _POOL_ID_WETH;
+
+    //     register(
+    //         testPortal, 
+    //         testAsset, 
+    //         testVault, 
+    //         testPid
+    //     );
+
+    //     // Validate the outcome
+    //     assert(virtualLP.registeredPortals(testPortal) == true);
+    //     assert(virtualLP.vaults(testPortal, testAsset) == testVault);
+    //     assert(virtualLP.poolID(testPortal, testAsset) == testPid);    
+    // }
+
+    // function test_revert_register_portal_not_owner() public {
+    //     address testPortal = address(portal_ETH);
+    //     address testAsset = _PRINCIPAL_TOKEN_ADDRESS_ETH; 
+    //     address testVault = address(WETH_WATER);
+    //     uint256 testPid = _POOL_ID_WETH;
+        
+    //     hevm.prank(USER1);
+    //     try 
+    //         virtualLP.registerPortal(
+    //             testPortal, 
+    //             testAsset, 
+    //             testVault, 
+    //             testPid
+    //         )
+    //     {
+    //         assert(false);
+    //     } catch {
+    //         assert(true);
+    //     }
+    // }
+
     /**
-        REGISTER PORTAL
+        REMOVE OWNER
      */
     /////////////// UNIT TESTS ///////////////
-    function test_register_portal_usdc() public {
-        address testPortal = address(portal_USDC);
-        address testAsset = _PRINCIPAL_TOKEN_ADDRESS_USDC; 
-        address testVault = address(USDC_WATER);
-        uint256 testPid = _POOL_ID_USDC;
-
-        hevm.prank(psmSender);
-        virtualLP.registerPortal(
-            testPortal, 
-            testAsset, 
-            testVault, 
-            testPid
-        );
-
-        // Validate the outcome
-        assert(virtualLP.registeredPortals(testPortal) == true);
-        assert(virtualLP.vaults(testPortal, testAsset) == testVault);
-        assert(virtualLP.poolID(testPortal, testAsset) == testPid);    
-    }
-
-    function test_register_portal_eth() public {
-        address testPortal = address(portal_ETH);
-        address testAsset = _PRINCIPAL_TOKEN_ADDRESS_ETH; 
-        address testVault = address(WETH_WATER);
-        uint256 testPid = _POOL_ID_WETH;
-
-        hevm.prank(psmSender);
-        virtualLP.registerPortal(
-            testPortal, 
-            testAsset, 
-            testVault, 
-            testPid
-        );
-
-        // Validate the outcome
-        assert(virtualLP.registeredPortals(testPortal) == true);
-        assert(virtualLP.vaults(testPortal, testAsset) == testVault);
-        assert(virtualLP.poolID(testPortal, testAsset) == testPid);    
-    }
-
-    function test_register_portal_only_owner() public {
-        address testPortal = address(portal_ETH);
-        address testAsset = _PRINCIPAL_TOKEN_ADDRESS_ETH; 
-        address testVault = address(WETH_WATER);
-        uint256 testPid = _POOL_ID_WETH;
+    function test_address_changed_to_zero() public {
+        hevm.warp(block.timestamp + OWNER_DURATION + 1);
+        address ownerBefore = virtualLP.owner.address;
+        Debugger.log("Owner address before:", ownerBefore);
+        Debugger.log("Owner address before:", psmSender);
         
-        hevm.prank(USER1);
-        try 
-            virtualLP.registerPortal(
-                testPortal, 
-                testAsset, 
-                testVault, 
-                testPid
-            )
-        {
+        virtualLP.removeOwner();
+        Debugger.log("Owner address after:", virtualLP.owner.address);
+        assert(virtualLP.owner.address == address(0));
+    }
+
+    function test_revert_remove_owner() public {
+        hevm.warp(OWNER_DURATION - 10);
+        address ownerBefore = virtualLP.owner.address;
+        Debugger.log("Owner address before:", ownerBefore);
+        try virtualLP.removeOwner() {
             assert(false);
         } catch {
             assert(true);
